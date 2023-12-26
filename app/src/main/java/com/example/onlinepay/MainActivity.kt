5a56d8510a09d8065a9305fc10c03c250d29d677
Madhav <madhav09solanki@gmail.com>
2023-12-26 15:21:11 +0530

    Second Commit

--- app/src/main/java/com/example/onlinepay/MainActivity.kt
+++ app/src/main/java/com/example/onlinepay/MainActivity.kt
@@ -1,17 +1,41 @@
 package com.example.onlinepay
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.example.onlinepay.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // App Created
+        auth = Firebase.auth
+
+        val user = auth.currentUser
+
+        if (user == null){
+            Handler(Looper.getMainLooper()).postDelayed({
+                startActivity(Intent(this@MainActivity, LoginScreen::class.java))
+                finish()
+            },2000)
+        }
+
+        else{
+            Handler(Looper.getMainLooper()).postDelayed({
+                startActivity(Intent(this@MainActivity, HomeScreen::class.java))
+                finish()
+            },2000)
+        }
+
     }
 }
