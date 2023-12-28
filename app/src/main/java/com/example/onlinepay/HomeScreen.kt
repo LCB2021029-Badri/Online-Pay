f1424e5a0bb041b9e54246eceb538edaf6fb8f99
badriakkala <lcb2021029@iiitl.ac.in>
2023-12-28 17:13:30 +0530

    home page edit

--- app/src/main/java/com/example/onlinepay/HomeScreen.kt
+++ app/src/main/java/com/example/onlinepay/HomeScreen.kt
@@ -1,11 +1,22 @@
 package com.example.onlinepay
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.onlinepay.databinding.ActivityHomeScreenBinding
 
 class HomeScreen : AppCompatActivity() {
+
+    private lateinit var binding:ActivityHomeScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home_screen)
+        binding = ActivityHomeScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnAeps.setOnClickListener {
+            startActivity(Intent(this@HomeScreen, AEPSActivity::class.java))
+        }
+
     }
 }
